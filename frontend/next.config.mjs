f53d3dc6fa7de906cf33a95c9e5811977f9939f8
Prince Kumar <sharmaprince2287@gmail.com>
2026-06-18 19:54:59 +0530

    fix: next config syntax error

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -16,6 +16,7 @@ const nextConfig = {
     NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
     NEXT_PUBLIC_WHATSAPP_NUMBER: process.env.NEXT_PUBLIC_WHATSAPP_NUMBER,
     NEXT_PUBLIC_RAZORPAY_KEY_ID: process.env.RAZORPAY_KEY_ID,
+  },
 };
 
 export default nextConfig;
